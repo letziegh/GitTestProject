267f74f060ab9bb5d2dbef0e67dc429781b11227
letziegh <148257470+letziegh@users.noreply.github.com>
2023-10-17 14:01:55 -0400

    Updated Profile with 2.0 changes

--- src/com/coderscampus/domain/Profile.java
+++ src/com/coderscampus/domain/Profile.java
@@ -1,5 +1,11 @@
 package com.coderscampus.domain;
 
+import java.time.LocalDate;
+
 public class Profile {
+	private Integer id;
+	private String name;
+	private String url;  
+	private LocalDate createdDate;
 
 }
